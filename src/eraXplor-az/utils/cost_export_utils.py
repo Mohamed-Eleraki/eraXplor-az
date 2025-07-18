6d50566a75e4ffa2fe0ebdfcf662e9a1f69dbc2a
mohamed eraki <mohamed-ibrahim2021@outlook.com>
2025-07-18 17:14:16 +0300

    monthly_sub_cost_export return a list

--- src/eraXplor-az/utils/cost_export_utils.py
+++ src/eraXplor-az/utils/cost_export_utils.py
@@ -8,6 +8,7 @@ def monthly_sub_cost_export(
     start_date: str, 
     end_date: str,
 ) -> dict:
+    
     credential = DefaultAzureCredential()
     cm_client = CostManagementClient(credential)
     
@@ -15,25 +16,43 @@ def monthly_sub_cost_export(
     end_date = datetime.datetime.strptime(end_date, "%Y,%m,%d")
     scope = f"/subscriptions/{subscription_id}"
     
-    cm_client_query = cm_client.query.usage(
-        scope=scope,
-        parameters=models.QueryDefinition(
-            type='Usage',
-            timeframe='Custom',
-            time_period=models.QueryTimePeriod(
-                from_property=start_date,
-                to=end_date,
-            ),
-            dataset=models.QueryDataset(
-                granularity='Daily',
-                aggregation={
-                    'totalcost': models.QueryAggregation(name='PreTaxCost', function='Sum')
-                }
+    cm_client_query_results = []
+        
+    try:
+        cm_client_query = cm_client.query.usage(
+            scope=scope,
+            parameters=models.QueryDefinition(
+                type='Usage',
+                timeframe='Custom',
+                time_period=models.QueryTimePeriod(
+                    from_property=start_date,
+                    to=end_date,
+                ),
+                dataset=models.QueryDataset(
+                    granularity='Daily',
+                    aggregation={
+                        'totalcost': models.QueryAggregation(name='PreTaxCost', function='Sum')
+                    }
+                )
             )
         )
-    )
-    cm_client_query_result = cm_client_query.rows
-    print(cm_client_query_result)
-    
-monthly_sub_cost_export("pass_sub_id_here", "2025,1,1", "2025,6,30")
-# print(monthly_sub_cost_export)
+        cm_client_query_rows = cm_client_query.rows
+        for row in cm_client_query_rows:
+            time_period = row[1]
+            cost = row[0]
+            currency = row[2]
+            
+            cm_client_query_results.append(
+                {
+                    "TIME_PERIOD": time_period,
+                    "COST": f"{cost:.2f} {currency}",
+                }
+            )
+            
+        return cm_client_query_results
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {"error": str(e)}
+
+cm_client_query_results = monthly_sub_cost_export("sub_id_here", "2025,1,1", "2025,4,30")
+print(cm_client_query_results)
